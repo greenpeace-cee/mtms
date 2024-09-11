1fec29aee708fe82b95d5fa8c42318700af48928
Patrick Figel <pfigel@greenpeace.org>
2024-09-11 21:06:33 +0200

    Upgrade civix

--- mtms.php
+++ mtms.php
@@ -15,15 +15,6 @@ function mtms_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function mtms_civicrm_xmlMenu(&$files) {
-  _mtms_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -42,15 +33,6 @@ function mtms_civicrm_install() {
     ->setCheckPermissions(FALSE)
     ->execute();
   _mtms_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function mtms_civicrm_postInstall() {
-  _mtms_civix_civicrm_postInstall();
 }
 
 /**
@@ -73,7 +55,6 @@ function mtms_civicrm_uninstall() {
     'name'                   => 'mtms',
     'api.SmsProvider.delete' => [],
   ]);
-  _mtms_civix_civicrm_uninstall();
 }
 
 /**
@@ -177,82 +158,6 @@ function mtms_civicrm_disable() {
     'is_active'              => 1,
     'api.SmsProvider.create' => ['is_active' => 0],
   ]);
-  _mtms_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function mtms_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _mtms_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function mtms_civicrm_managed(&$entities) {
-  _mtms_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function mtms_civicrm_caseTypes(&$caseTypes) {
-  _mtms_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function mtms_civicrm_angularModules(&$angularModules) {
-  _mtms_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function mtms_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _mtms_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function mtms_civicrm_entityTypes(&$entityTypes) {
-  _mtms_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function mtms_civicrm_themes(&$themes) {
-  _mtms_civix_civicrm_themes($themes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
